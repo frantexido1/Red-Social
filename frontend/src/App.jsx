d6e92ae3ec6ac5ad7489c0764973dda6556afd07
Yashar Ghafarinejad <yasharg97@gmail.com>
2024-02-19 22:39:20 -0500

    segundo commit de prueba

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 
 import Login from './paginas/Login';
+import Register from './paginas/Register';
 
 const App = () => {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path='/' element={<Login />} />
+                <Route path='/register' element={<Register />} />
             </Routes>
         </BrowserRouter>
     )
