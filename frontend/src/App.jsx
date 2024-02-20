472a5d3f2999e7d2a03dee70e590f6646a40e4c9
Yashar Ghafarinejad <yasharg97@gmail.com>
2024-02-19 22:35:18 -0500

    commit de prueba

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+
+import Login from './paginas/Login';
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Login />} />
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
 export default App
